24cabb6fcd0c111087c4026b500c93a710bf4bb7
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-01-02 14:21:15 +0900

    finish step2 : add library function

--- src/01/bootload/main.c
+++ src/01/bootload/main.c
@@ -6,6 +6,10 @@ int main(void)
 {
     serial_init(SERIAL_DEFAULT_DEVICE);
     puts("Hello World\n");
+    putxval(0x10, 0);
+    puts("\n");
+    putxval(0xffff, 0);
+    puts("\n");
     while (1);
     return 0;
 }
